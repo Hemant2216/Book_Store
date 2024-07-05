ed6a9838096cd3a9b29205640af009d420ccb465
Hemant2216 <88074414+Hemant2216@users.noreply.github.com>
2024-07-05 09:44:24 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,3 @@
-export const PORT='https://book-store-backend-jet.vercel.app/';
+export const PORT=process.env.PORT || 5555;
 
 export const mongoDBURL='mongodb+srv://root:root@mernbookstore.tjmqali.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERNBookstore'
