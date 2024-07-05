154ac6e280a6efc540a048ff163a04e44d9dc410
Hemant2216 <88074414+Hemant2216@users.noreply.github.com>
2024-07-05 09:19:13 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     useEffect(()=>{
         setLoading(true);
         axios
-            .get('https://book-store-api-gray-ten.vercel.app/books')
+            .get('https://book-store-backend-jet.vercel.app/books')
             .then((response)=>{
                 setBooks(response.data.data);
                 setLoading(false);
