9e80c7cdfbadf2a7fad392e75ab9d1cdf1d4b3b0
Hemant2216 <hemantdahiya44@gmail.com>
2024-07-05 08:12:18 +0530

    first commit

---
+++ Backend/config.js
@@ -0,0 +1,3 @@
+export const PORT=5555;
+
+export const mongoDBURL='mongodb+srv://root:root@mernbookstore.tjmqali.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERNBookstore'
---
+++ Backend/index.js
@@ -0,0 +1,44 @@
+import express from "express"; //importing express
+import {PORT,mongoDBURL} from "./config.js"; //importing PORT
+import mongoose from "mongoose";
+import {Book} from './models/bookModels.js';
+import booksRoute from './routes/booksroute.js';
+import cors from 'cors';
+
+
+const app=express(); //storing it so that we can use
+app.use(express.json());//middleware for parsing json body postman 
+
+app.get('/',(req,res)=>{
+    console.log(req);
+    return res.status(234).send('welcome');
+});
+
+
+
+//middleware for handling CORS policy
+// app.use(
+//     cors({
+//         origin:'http://localhost:5555/books',
+//         methods:['GET','POST','PUT','DELETE'],
+//         allowedHeaders:['Access-Control-Allow-Origin'],
+//     })
+// )
+app.use(cors());
+
+//midleware for crud using express
+app.use('/books',booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log('App connected to database');
+        // function listen to port and ensure server is running
+        app.listen(PORT,()=>{
+            console.log(`App is listening at: ${PORT}`);
+        });
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+
